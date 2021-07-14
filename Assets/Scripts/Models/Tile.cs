bd33db74ff45179e9399f73fbcffdbb655117831
Stefan Tataran <fane_ecl@yahoo.com>
2021-07-14 18:02:06 +0300

    Added tile data and some logic

--- Assets/Scripts/Models/Tile.cs
+++ Assets/Scripts/Models/Tile.cs
@@ -1,18 +1,101 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
-public class Tile : MonoBehaviour
+/// <summary>
+/// Model for a tile, the building blocks of the world.
+/// </summary>
+public class Tile
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// A list of functions to be called when the <see cref="Type"/> of the tile is changed.
+    /// </summary>
+    private Action<Tile> cbTileChanged;
+
+    private TileType _type = TileType.Water;
+
+    public TileType Type
     {
-        
+        get => _type;
+        set
+        {
+            var oldType = _type;
+            _type = value;
+
+            if (cbTileChanged != null && oldType != _type)
+            {
+                cbTileChanged(this);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int X { get; protected set; }
+    public int Y { get; protected set; }
+
+    // TODO: This is just hardcoded for now. Basically just a reminder of something we
+    // might want to do more with in the future.
+    private const float baseTileMovementCost = 1;
+
+    /// <summary>
+    /// The cost of moving to this tile. 0 means the tile is not walkable.
+    /// </summary>
+    public float MovementCost
     {
-        
+        get
+        {
+            // TODO: Return 0 for unwalkable tiles types.
+            // TODO: Modify speed based on furniture/objects placed on the tile.
+            return baseTileMovementCost;
+        }
     }
+
+    /// <summary>
+    /// See <see cref="Enterability"/>.
+    /// </summary>
+    public Enterability Enterability
+    {
+        get
+        {
+            if (MovementCost == 0)
+            {
+                return Enterability.NotEnterable;
+            }
+
+            // TODO: Add logic for when furniture/objects/characters are on the tile.
+
+            return Enterability.Enterable;
+        }
+    }
+
+    /// <summary>
+	/// Register a function to be called back when the <see cref="Type"/> changes.
+	/// </summary>
+	public void SubscribeTileTypeChanged(Action<Tile> callback) => cbTileChanged += callback;
+
+    /// <summary>
+	/// Unregister a function from <see cref="cbTileChanged"/> callback.
+    /// </summary>
+    public void UnsubscribeTileTypeChanged(Action<Tile> callback) => cbTileChanged -= callback;
+}
+
+/// <summary>
+/// Represents all the tile types supported by the game.
+/// </summary>
+public enum TileType
+{
+    Grass,
+    Water
+}
+
+/// <summary>
+/// Represents wether a character can enter the tile or not.
+/// </summary>
+/// <remarks>
+/// <see cref="Enterability.Enterable"/> The tile can be entered right now.
+/// <see cref="Enterability.NotEnterable"/> The tile can never be entered.
+/// <see cref="Enterability.Busy"/> The tile can be entered after the furniture/object/character on the tile has finished its job.
+/// </remarks>
+public enum Enterability
+{
+    Enterable,
+    NotEnterable,
+    Busy
 }
